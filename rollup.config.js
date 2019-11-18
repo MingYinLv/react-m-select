05021cdb08d033a99a73864ca9fee71d57b5dd65
lvmingyin <lvmingyin@vip.qq.com>
2019-11-18 17:05:46 +0800

    fist

---
+++ rollup.config.js
@@ -0,0 +1,30 @@
+/**
+ * Created 2019/11/18 14:57 By lvmingyin
+ */
+import commonjs from 'rollup-plugin-commonjs';
+import typescript from 'rollup-plugin-typescript2';
+import url from 'rollup-plugin-url';
+import postcss from 'rollup-plugin-postcss';
+
+export default {
+  input: 'src/main.ts',
+  output: {
+    file: 'dist/index.js',
+    format: 'iife',
+    name: 'version',
+  },
+  watch: {
+    include: 'src/**',
+  },
+  plugins: [
+    url(),
+    postcss({
+      extensions: ['.less', '.css'],
+      modules: true,
+    }),
+    commonjs(),
+    typescript({
+      clean: true,
+    }),
+  ],
+};
---
+++ src/main.ts
@@ -0,0 +1,9 @@
+/**
+ * Created 2019/11/18 14:54 By lvmingyin
+ */
+
+import React from 'react';
+
+export default function() {
+
+}
