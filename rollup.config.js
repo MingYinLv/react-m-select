7e53635c939566245912895b5de03ecef01257e8
lvmingyin <lvmingyin@vip.qq.com>
2019-11-20 10:25:32 +0800

    add es

--- rollup.config.js
+++ rollup.config.js
@@ -5,23 +5,28 @@ import commonjs from 'rollup-plugin-commonjs';
 import typescript from 'rollup-plugin-typescript2';
 import url from 'rollup-plugin-url';
 import postcss from 'rollup-plugin-postcss';
+import pkg from 'package';
 
 export default {
   input: 'src/index.ts',
-  output: {
-    file: 'dist/index.js',
-    format: 'iife',
-    name: 'version',
-  },
+  output: [
+    {
+      file: pkg.main,
+      format: 'cjs',
+      name: 'version',
+    },
+    {
+      file: pkg.module,
+      format: 'es',
+      name: 'version',
+    },
+  ],
   watch: {
     include: 'src/**',
   },
   plugins: [
     url(),
-    postcss({
-      extensions: ['.less', '.css'],
-      modules: true,
-    }),
+    postcss({ extract: `dist/index.css` }),
     commonjs(),
     typescript({
       clean: true,
